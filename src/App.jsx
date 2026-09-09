a9b150d551b5a7eb8c1624242a2b62b522ce1d92
mahlet-tesfahunegn <mahlettesfahunegn@gmail.com>
2026-09-09 05:31:11 -0700

    feat:Build and style amazon hero carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from './Components/Header/Header'
+import CarouselEffect from './Components/Carousel/Carousel'
 import './App.css'
 
 function App() {
   return (
     <div>
       <Header/>
+      <CarouselEffect/>
     </div>
   )
 }
 
 export default App
+
---
+++ src/Components/Carousel/img/data.js
@@ -0,0 +1,6 @@
+import img1 from './gaming.jpg'
+import img2 from './kithcen.jpg'
+import img3 from './school.jpg'
+import img4 from './toys.jpg'
+
+export const img = [img1, img2, img3, img4]
