d7717131a8df6fcf064755886a30fc007e033bb7
mahlet-tesfahunegn <mahlettesfahunegn@gmail.com>
2026-09-13 11:58:35 -0700

    feat: Build the category section structure

--- src/Components/Category/categoryInfo.js
+++ src/Components/Category/categoryInfo.js
@@ -2,25 +2,53 @@ export const categoryInfos = [
     {
         title:"Must have for every student",
         name:"must have for every student",
-        imgLink:"https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2026/BTS2026/Fuji_SingleImageCard_BTS_SH26_2x._SY608_CB788455970_.jpg",
+        imgLink:"../../../images-and-data-source-content-1747788329838/school.png",
         shop:"Shop Back to School",
     },
     {
         title:"Get your game on",
         name:"get your game on",
-        imgLink:"https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2026/Gaming_26/Fuji_Gaming_SingleImageCard_C._SY608_CB762596570_.jpg",
+        imgLink:"../../../images-and-data-source-content-1747788329838/game.png",
         shop: "Shop gaming",
     },
     {
         title:"Shop fashion for less",
         name:"shop fashion for less",
-        imgLink:"https://images-na.ssl-images-amazon.com/images/G/01/AMAZON_FASHION/2022/SITE_FLIPS/SPR_22/GW/DQC/DQC_APR_TBYB_W_SHOES_2x._SY232_CB624172947_.jpg",
-        shop:"Shop all deals",
+        items:[
+            { id: "f1", title: "Jeans under $50", image: "/img/jeans.jpg", alt: "Folded jeans" },
+            { id: "f2", title: "Tops under $25", image: "/img/sweaters.jpg", alt: "Tops" },
+            { id: "f3", title: "Dresses under $30", image: "/img/dresses.jpg", alt: "Dresses" },
+            { id: "f4", title: "Shoes under $50", image: "/img/shoes.jpg", alt: "Boots" }
+        ]
     },
     {
         title:"Must-have schoold supplies",
         name:"must-have schoold supplies",
         imgLink:"https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2025/BTS25/GenAIExperiment/Group_C/Fuji_QuadCard_Electronics_BTS25_2x._SY232_CB789327598_.jpg",
         shop:"Shop Back to School",
-    }
+    },
+    {
+        title:"Top categories in Kitchen appliances",
+        name:"must have for every student",
+        imgLink:"../../../images-and-data-source-content-1747788329838/school.png",
+        shop:"Explore all products in Kitchen",
+    },
+    {
+        title:"New home arrivals under $50",
+        name:"must have for every student",
+        imgLink:"../../../images-and-data-source-content-1747788329838/school.png",
+        shop:"Shop the latest from Home",
+    },
+    {
+        title:"Fashion trends you like",
+        name:"must have for every student",
+        imgLink:"../../../images-and-data-source-content-1747788329838/school.png",
+        shop:"Explore more",
+    },
+    {
+        title:"Easy updates for elevated spaces",
+        name:"must have for every student",
+        imgLink:"../../../images-and-data-source-content-1747788329838/school.png",
+        shop:"Shop home products",
+    },
 ]
